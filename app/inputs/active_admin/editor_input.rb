d648d9c64fb2e36fba40550944dc5aee8d7c821c
Eric J. Holmes <eric@ejholmes.net>
2012-04-30 13:19:25 -0700

    Add formtastic input.

---
+++ app/inputs/active_admin/editor_input.rb
@@ -0,0 +1,5 @@
+class ActiveAdmin::EditorInput < Formtastic::Inputs::TextInput
+  def to_html
+    super
+  end
+end
--- test/dummy/app/admin/pages.rb
+++ test/dummy/app/admin/pages.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Page do
-  
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :content, as: :editor
+    end
+
+    f.buttons
+  end
 end
