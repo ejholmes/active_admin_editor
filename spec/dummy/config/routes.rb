3c79f3af3703961dca544e81a6feed85c3384376
Eric J. Holmes <eric@ejholmes.net>
2012-12-24 12:04:44 -0800

    Add activeadmin to dummy app.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,8 @@
 Dummy::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
