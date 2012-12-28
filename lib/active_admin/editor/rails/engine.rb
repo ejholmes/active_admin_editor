3f8890249dbe69672725f3ce9d9c7fc7c304a592
Eric J. Holmes <eric@ejholmes.net>
2012-12-27 22:58:43 -0800

    Precompile editor assets.

--- lib/active_admin/editor/rails/engine.rb
+++ lib/active_admin/editor/rails/engine.rb
@@ -5,6 +5,8 @@ module ActiveAdmin
         engine_name 'active_admin_editor'
 
         initializer 'active_admin.editor' do |app|
+          app.config.assets.precompile += %w(active_admin/editor.js active_admin/editor.css active_admin/editor/wysiwyg.css)
+
           ActiveAdmin.setup do |config|
             config.register_javascript 'active_admin/editor.js'
             config.register_stylesheet 'active_admin/editor.css'
