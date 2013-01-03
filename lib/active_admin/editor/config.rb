c37b419ddcc5df00d13a2af3f6a32d82c452fccb
Eric J. Holmes <eric@ejholmes.net>
2013-01-02 22:42:26 -0800

    Allow wysiwyg stylesheet to be configurable.

--- lib/active_admin/editor/config.rb
+++ lib/active_admin/editor/config.rb
@@ -20,12 +20,19 @@ module ActiveAdmin
       attr_accessor :s3_bucket
       attr_accessor :storage_dir
 
+      # wysiwyg stylesheets that get included in the backend and the frontend.
+      attr_accessor :stylesheets
+
       def storage_dir
         @storage_dir ||= 'uploads'
       end
 
       def storage_dir=(dir)
         @storage_dir = dir.to_s.gsub(/(^\/|\/$)/, '')
+      end
+
+      def stylesheets
+        @stylesheets ||= [ 'active_admin/editor/wysiwyg.css' ]
       end
 
       def s3_configured?
--- lib/active_admin/editor/engine.rb
+++ lib/active_admin/editor/engine.rb
@@ -10,14 +10,16 @@ module ActiveAdmin
       initializer 'active_admin.editor' do |app|
         app.config.assets.precompile += [
           'active_admin/editor.js',
-          'active_admin/editor.css',
-          'active_admin/editor/wysiwyg.css'
-        ]
+          'active_admin/editor.css'
+        ] + ActiveAdmin::Editor.configuration.stylesheets
 
         ActiveAdmin.setup do |config|
           config.register_javascript 'active_admin/editor.js'
           config.register_stylesheet 'active_admin/editor.css'
-          config.register_stylesheet 'active_admin/editor/wysiwyg.css'
+
+          ActiveAdmin::Editor.configuration.stylesheets.each do |stylesheet|
+            config.register_stylesheet stylesheet
+          end
         end
       end
     end
