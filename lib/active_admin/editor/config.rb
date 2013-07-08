e33c42e232ed82af2a22575664fd537ee90b22c7
Lukas Hodel <l.hodel@de.edenspiekermann.com>
2013-07-08 18:02:24 +0200

    Adds config var for uploader_action_path

--- lib/active_admin/editor/config.rb
+++ lib/active_admin/editor/config.rb
@@ -29,6 +29,9 @@ module ActiveAdmin
       # wysiwyg stylesheets that get included in the backend and the frontend.
       attr_accessor :stylesheets
 
+      # action which should handle file upload
+      attr_accessor :uploader_action_path
+
       def storage_dir
         @storage_dir ||= 'uploads'
       end
@@ -49,6 +52,10 @@ module ActiveAdmin
 
       def parser_rules
         @parser_rules ||= PARSER_RULES.dup
+      end
+
+      def uploader_action_path=(action)
+        @uploader_action_path = (action.nil?) ? action : "/#{ action.to_s.gsub(/(^\/|\/$)/, '') }"
       end
     end
   end
