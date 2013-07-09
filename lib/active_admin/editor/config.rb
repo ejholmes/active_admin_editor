919f8ebed1261299e7ddac0cee681514bca22fa8
Lukas Hodel <l.hodel@de.edenspiekermann.com>
2013-07-09 13:27:40 +0200

    Fixes wysiwyg.css in config, typo got in by accident

--- lib/active_admin/editor/config.rb
+++ lib/active_admin/editor/config.rb
@@ -41,7 +41,7 @@ module ActiveAdmin
       end
 
       def stylesheets
-        @stylesheets ||= [ 'active_admin/editor/wymiwyg.css' ]
+        @stylesheets ||= [ 'active_admin/editor/wysiwyg.css' ]
       end
 
       def s3_configured?
