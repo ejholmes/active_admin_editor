87e7d17597774c71e02663f6800ef6211d79e9bf
Marius Andra <marius.andra@gmail.com>
2013-08-21 13:42:24 +0200

    wysihtml5 size matters

--- app/assets/javascripts/active_admin/editor.js
+++ app/assets/javascripts/active_admin/editor.js
@@ -1,7 +1,11 @@
 //= require wysihtml5
 //= require active_admin/editor/config
 //= require active_admin/editor/editor
+//= require active_admin/editor/jquery.wysihtml5_size_matters.js
 
 ;(function(window, $) {
-  $(function() { $('.html_editor').editor(window.AA.editor_config) })
-})(window, jQuery)
+  $(function() { 
+    $('.html_editor').editor(window.AA.editor_config);
+    $('iframe.wysihtml5-sandbox').wysihtml5_size_matters();
+  });
+})(window, jQuery);
